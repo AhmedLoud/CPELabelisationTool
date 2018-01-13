595422034cf39404b476e2e4a87d673598aeeb9a
AhmedLoud <ahmed.loudghiri@gmail.com>
2018-01-13 19:02:53 +0000

    started drawing of bounding box more to come

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -1,37 +1,101 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { ImageToLabelise } from '../models/index'
+import { ImageToLabelise, BoundingBox } from '../models/index'
+
+class MousePos {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
   styleUrls: ['./image.component.css']
 })
+
+
 export class ImageComponent implements OnInit {
 
   image: ImageToLabelise = {
     id: 1,
     imageUrl: 'https://source.unsplash.com/random',
-    imagePath: 'https://source.unsplash.com/random'
-  }
+    imagePath: 'https://source.unsplash.com/random',
+    boundingBoxs: []
+  };
+  currentBoundingBox: BoundingBox;
+  isDrawingBoundingBox: boolean;
+
   @ViewChild("imageCanvas") imageCanvas: ElementRef;
   constructor() {
+    this.currentBoundingBox = new BoundingBox();
+  }
+
+  ngOnInit(): void {
 
   }
 
-  ngOnInit() {
+  /**
+   * Function getMousePos
+   * **********************
+   * Return the relative coordinate of a mouse event in the HTML canvas
+   * @param event 
+   */
+  getMousePos(event: MouseEvent): MousePos {
+    let rect = this.imageCanvas.nativeElement.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    const pos: MousePos = new MousePos();
+    pos.x = x;
+    pos.y = y;
+    return pos;
   }
 
-  ngAfterViewInit() {
-    console.log('test');
+  ngAfterViewInit(): void {
     let context: CanvasRenderingContext2D = this.imageCanvas.nativeElement.getContext("2d");
     const randomImage = new Image();
-    randomImage.src = 'https://source.unsplash.com/random';
+    randomImage.src = this.image.imageUrl;
     randomImage.onload = () => {
       context.canvas.height = randomImage.height;
       context.canvas.width = randomImage.width;
-      context.drawImage(randomImage, 0,0);
+      context.drawImage(randomImage, 0, 0);
+
+      /*
+      Event Listener when mouse is down on the HTML canvas
+       */
+      this.imageCanvas.nativeElement.addEventListener('mousedown', (event: MouseEvent) => {
+        if (!this.isDrawingBoundingBox) {
+          const pos = this.getMousePos(event);
+          this.isDrawingBoundingBox = true;
+          this.currentBoundingBox.x = pos.x;
+          this.currentBoundingBox.y = pos.y;
+        }
+
+      });
+
+      this.imageCanvas.nativeElement.addEventListener('mousemove', (event: MouseEvent) => {
+        if (this.isDrawingBoundingBox) {
+          const pos = this.getMousePos(event);
+          this.currentBoundingBox.w = Math.abs(pos.x - this.currentBoundingBox.x);
+          this.currentBoundingBox.h = Math.abs(pos.y - this.currentBoundingBox.y);
+        }
+      });
+
+      this.imageCanvas.nativeElement.addEventListener('mouseup', (event: MouseEvent) => {
+        if (this.isDrawingBoundingBox) {
+          this.isDrawingBoundingBox = false;
+
+        }
+      })
+
+      setInterval(() => {
+        context.clearRect(0,0,context.canvas.width, context.canvas.height);
+        context.drawImage(randomImage, 0,0);   
+        context.fillRect(this.currentBoundingBox.x, this.currentBoundingBox.y,
+          this.currentBoundingBox.w, this.currentBoundingBox.h);
+      });
     };
 
   }
+
+
 
 }
--- src/app/models/image-to-labelise.ts
+++ src/app/models/image-to-labelise.ts
@@ -3,5 +3,5 @@ export class ImageToLabelise {
   id: number;
   imagePath: string;
   imageUrl: string;
-
+  boundingBoxs: BoundingBox[];
 }
