9543df2656b9c41f64cfe38d4374bba13d402c92
AhmedLoud <ahmed.loudghiri@gmail.com>
2018-01-23 22:58:43 +0000

    finished label admin component

--- src/app/admin/labels/labels.component.ts
+++ src/app/admin/labels/labels.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LabelService } from '../../services/label.service';
+import { Label } from '../../models/index';
 
 @Component({
   selector: 'app-labels',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LabelsComponent implements OnInit {
 
-  constructor() { }
+  labels: Label[];
+  hoverIndex: number;
+  constructor(private labelService: LabelService) { }
 
   ngOnInit() {
+    this.getLabels();
+  }
+
+  getLabels(): void {
+    this.labelService.getLabels().subscribe(labels => {
+      this.labels = labels;
+    });
+  }
+
+  addLabel(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.labelService.createLabel({ name } as Label).subscribe(label => {
+      this.labels.push(label);
+    })
+  }
+
+  onDeleteLabel(label: Label): void {
+    this.labels = this.labels.filter(l => l !== label);
+    this.labelService.deleteLabel(label).subscribe();
   }
 
 }
--- src/app/services/label.service.ts
+++ src/app/services/label.service.ts
@@ -13,6 +13,10 @@ export const LABELS: Label[] = [
   { id: 5, name: 'cosmetique' }
 ];
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
+
 @Injectable()
 export class LabelService {
   private backendUrl = 'http://localhost:3000/ping';
@@ -27,5 +31,16 @@ export class LabelService {
 
   ping(): Observable<string> {
     return this.http.get<string>(this.backendUrl);
+  }
+
+  deleteLabel(label: Label): Observable<Label> {
+    const id: number = label.id;
+    const url: string = this.settingsService.backendApiUrl + `/labels/${id}`;
+    return this.http.delete<Label>(url, httpOptions);
+  }
+
+  createLabel(label: Label): Observable<Label> {
+    const url: string = this.settingsService.backendApiUrl + '/labels';
+    return this.http.post<Label>(url, label, httpOptions);
   }
 }
